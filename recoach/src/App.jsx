9a3476d9cc49eec17252b15a343286fbacb1f0ec
BusherBridi <bridibusher@gmail.com>
2025-07-16 02:13:44 -0700

    project init

---
+++ recoach/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-base-200">
+      <div className="card w-full max-w-xl shadow-xl bg-base-100 text-base-content">
+        <div className="card-body">
+          <h1 className="card-title text-3xl justify-center mb-4">⚽ CrackHeads FC</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
