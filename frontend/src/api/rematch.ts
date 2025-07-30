ab52a871d3bd9a0884a8f35cf499c473f5257474
BusherBridi <bridibusher@gmail.com>
2025-07-29 22:47:52 -0700

    migration try 1

--- frontend/src/api/rematch.ts
+++ frontend/src/api/rematch.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://api.rematchtracker.com/scrap/profile";
+const API_URL = "http://localhost:5000/api/player-profile";
 
 export async function fetchPlayerProfile(platform: string, platformId: string) {
   try {
