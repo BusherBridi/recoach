006999812e1103a9c79008c4965a3e11a3e64ad3
BusherBridi <bridibusher@gmail.com>
2025-08-12 22:39:41 -0700

    forgot to add route to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import playerRoutes from "./routes/playerProfile.routes";
+import teamRoutes from "./routes/teamProfile.routes";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", playerRoutes);
+app.use("/api", teamRoutes);
 
 export default app;
