688e8107b8b139d83d5a26812191338a99ece9f2
BusherBridi <bridibusher@gmail.com>
2025-08-12 22:42:50 -0700

    i need a dev server...

--- backend/src/controllers/teamProfile.controller.ts
+++ backend/src/controllers/teamProfile.controller.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
-import { getTeamProfile} from "../services/rematchAPI.service";
+import { getTeamMatches } from "../services/rematchAPI.service";
 
 export async function fetchTeamProfile(req: Request, res: Response) {
   try {
-    const { platform, platformId } = req.body;
-    const data = await getTeamProfile(platform, platformId);
+    const { platform, platformIds } = req.body; // Accept array of IDs
+    const data = await getTeamMatches(platform, platformIds); // Call the correct service
     res.json(data);
   } catch (error) {
     console.error("Controller error:", error);
-    res.status(500).json({ error: "Failed to fetch player profile" });
+    res.status(500).json({ error: "Failed to fetch team profile" });
   }
 }
